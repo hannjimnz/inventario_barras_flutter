c5607424019a783454ca799a122be5189de35797
Jazmin Gutierrez <jazmingut9@gmail.com>
2026-07-29 14:02:28 -0600

    logica de entradas y salidas del inventario

--- lib/screens/entrada_screen.dart
+++ lib/screens/entrada_screen.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
+import 'package:provider/provider.dart';
+import 'package:codigo_barras/models/producto.dart';
+import 'package:codigo_barras/providers/inventario_provider.dart';
+import 'package:codigo_barras/screens/scanner_screen.dart';
+
+
 class EntradaScreen extends StatelessWidget {
 
   const EntradaScreen({super.key});
@@ -12,9 +18,51 @@ class EntradaScreen extends StatelessWidget {
         title: const Text("Entrada de productos"),
       ),
 
-      body: const Center(
-        child: Text(
-          "Pantalla de entrada",
+      body: Center(
+        child: ElevatedButton.icon(
+          onPressed: () async {
+              final codigo = await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => const ScannerScreen(),
+                ),
+              );
+              if (codigo == null){
+                return;
+              }
+              final producto = await context
+              .read<InventarioProvider>()
+              .buscarProducto(codigo);
+
+              if (producto == null){
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text("Producto NO encontrado"),
+                  )
+                  );
+                  return;
+              }
+              final productoActualizado = Producto(
+              id: producto.id,
+              codigo: producto.codigo,
+              nombre: producto.nombre,
+              descripcion: producto.descripcion,
+              precio: producto.precio,
+              existencia: producto.existencia + 1,
+              imagen: producto.imagen,
+            );
+            await context
+              .read<InventarioProvider>()
+              .actualizarProducto(productoActualizado);
+
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text("Se agregó una unidad de ${producto.nombre}"),
+            ),
+          );
+            },
+          icon: const Icon(Icons.camera_alt),
+          label: const Text("Escanear producto"),
         ),
       ),
     );
--- lib/screens/reporte_screen.dart
+++ lib/screens/reporte_screen.dart
@@ -6,64 +6,35 @@ import '../models/producto.dart';
 
 
 class ReporteScreen extends StatefulWidget {
-
   const ReporteScreen({super.key});
-
-
   @override
   State<ReporteScreen> createState() =>
       _ReporteScreenState();
 
 }
-
-
-
 class _ReporteScreenState extends State<ReporteScreen> {
-
-
   @override
   void initState() {
-
     super.initState();
-
-
     Future.microtask(() {
-
       context
           .read<InventarioProvider>()
           .cargarProductos();
-
     });
-
   }
-
-
 
   @override
   Widget build(BuildContext context) {
-
-
     final productos =
         context.watch<InventarioProvider>().productos;
-
-
-
     return Scaffold(
-
       appBar: AppBar(
-
         title: const Text(
           "Reporte de inventario",
         ),
-
         centerTitle: true,
-
       ),
-
-
-
       body: productos.isEmpty
-
           ? const Center(
 
               child: Text(
